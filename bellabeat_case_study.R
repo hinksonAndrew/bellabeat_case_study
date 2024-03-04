a2b9d0fc2a6ccfb5cdc7f2b7ad9cffb1e68fd2d5
hinksonAndrew <38588648+hinksonAndrew@users.noreply.github.com>
2024-03-04 17:05:31 -0600

    Added initial files

---
+++ bellabeat_case_study.R
@@ -0,0 +1,9 @@
+#load librarys
+library(tidyverse)
+
+#create data frames
+
+#daily_activity_4-12-16 thru 5-12-16
+daily_activity_april <- read.csv('Fitabase_data_4.12.16-5.12.16/dailyActivity_merged.csv')
+#sleep data 4-12-16 thru 5-12-16
+sleep_day_april <- read.csv('Fitabase_data_4.12.16-5.12.16/sleepDay_merged.csv')
